e32875475ac97262b9e7a14496ecffd4d9a5bbcf
Marco Vavassori <mv@marcovavassori.com>
2025-02-21 21:15:12 +0100

    method overload exception added

--- src/main/java/com/marcovavassori/banking/exceptions/UserNotFoundException.java
+++ src/main/java/com/marcovavassori/banking/exceptions/UserNotFoundException.java
@@ -4,4 +4,8 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long userId) {
         super("User not found with ID: " + userId);
     }
+
+    public UserNotFoundException(String email) {
+        super("User not found with email: " + email);
+    }
 }
