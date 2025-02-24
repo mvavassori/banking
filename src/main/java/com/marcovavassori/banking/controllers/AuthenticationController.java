c85123aad6af0cad61080523e3a806369ecfb456
Marco Vavassori <mv@marcovavassori.com>
2025-02-24 19:36:04 +0100

    Authentication controller

---
+++ src/main/java/com/marcovavassori/banking/controllers/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.marcovavassori.banking.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcovavassori.banking.dtos.AuthenticationResponse;
+import com.marcovavassori.banking.dtos.SignInRequest;
+import com.marcovavassori.banking.dtos.SignUpRequest;
+import com.marcovavassori.banking.services.AuthenticationService;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthenticationResponse> signUp(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signUp(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthenticationResponse> signIn(@RequestBody SignInRequest request) {
+        return ResponseEntity.ok(authenticationService.signIn(request));
+    }
+
+}
